f275dcb836996bb8242ccc7b1230999ddc08c406
O-Nicks <np.planche@gmail.com>
2025-10-16 20:54:57 +0200

    delete some uselessthings

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -16,7 +16,7 @@ export default function DefaultLayout({
       <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
         {children}
       </main>
-      <footer className="w-full flex text-xs items-center justify-center py-3">
+      <footer className="w-full flex text-xs items-center justify-center py-3 text-gray-300">
         <p>
           {" "}
           © {new Date().getFullYear()} Nicolas Planche - All rights
