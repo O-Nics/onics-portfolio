ebb981edc26aa98f42039b5974f95d765211e538
O-Nicks <np.planche@gmail.com>
2025-10-17 17:07:21 +0200

    spaces

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -14,7 +14,7 @@ export default function DefaultLayout({
       <Head />
       <Navbar />
       <main className="container-wrapper flex-grow lg:pl-72 mt-3 ">
-        <div className="border-gray-200 dark:border-white/10 border-dashed border-r lg:py-6 px-4 sm:px-6 lg:px-8">
+        <div className="border-gray-200 dark:border-white/10 border-dashed border-r lg:py-6 px-4 sm:px-6 lg:px-12">
           {children}
         </div>
       </main>
