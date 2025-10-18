f85480fb71cb3e6b66fefeac2792a4c4a956523a
O-Nicks <np.planche@gmail.com>
2025-10-18 13:22:46 +0200

    add intro, icons + link pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 "use client";
 import DefaultLayout from "@/layouts/default";
-
+import { NavigationInPage } from "@/components/navigationInPage";
+import { LinkNavigation } from "@/types";
 
 export default function Example() {
+  const rightLink: LinkNavigation = {
+    name: "À propos de moi",
+    href: "/a-propos",
+  };
 
   return (
     <>
@@ -16,9 +21,24 @@ export default function Example() {
       */}
       <DefaultLayout>
         <main className="">
-          
+          <h1 className="text-7xl font-extrabold">Nicolas Planche</h1>
+          <p className="text-3xl font-bold pt-2">
+            Développeur Full-Stack & Mobile
+          </p>
+          <p className="pt-4 text-xl">
+            Amoureux de la tech, je conçois des applications mobiles et web
+            ainsi que des back-ends solides.
+          </p>
+          <p className="pt-4 text-xl">
+            J’utilise des technologies modernes
+            tel que Flutter, Laravel, Vue/Nuxt, Next/React, Tailwind CSS et des
+            pratiques DevOps pour livrer des solutions logicielles complètes,
+            orientées utilisateur et de haute qualité.
+          </p>
+
+          <NavigationInPage right={rightLink} />
         </main>
       </DefaultLayout>
     </>
-  )
+  );
 }
