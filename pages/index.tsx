1ba9f9e142bea53cf81e9f3e6915c367f8d6d996
O-Nicks <np.planche@gmail.com>
2025-10-18 14:32:49 +0200

    animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@
 import DefaultLayout from "@/layouts/default";
 import { NavigationInPage } from "@/components/navigationInPage";
 import { LinkNavigation } from "@/types";
+import FadeUp from "@/components/animation/fade-up";
 
 export default function Example() {
   const rightLink: LinkNavigation = {
@@ -21,22 +22,32 @@ export default function Example() {
       */}
       <DefaultLayout>
         <main className="">
-          <h1 className="text-7xl font-extrabold">Nicolas Planche</h1>
-          <p className="text-3xl font-bold pt-2">
-            Développeur Full-Stack & Mobile
-          </p>
-          <p className="pt-4 text-xl">
-            Amoureux de la tech, je conçois des applications mobiles et web
-            ainsi que des back-ends solides.
-          </p>
-          <p className="pt-4 text-xl">
-            J’utilise des technologies modernes
-            tel que Flutter, Laravel, Vue/Nuxt, Next/React, Tailwind CSS et des
-            pratiques DevOps pour livrer des solutions logicielles complètes,
-            orientées utilisateur et de haute qualité.
-          </p>
+          <FadeUp>
+            <h1 className="text-7xl font-extrabold">Nicolas Planche</h1>
+          </FadeUp>
+          <FadeUp delay={0.1}>
+            <p className="text-3xl font-bold pt-2">
+              Développeur Full-Stack & Mobile
+            </p>
+          </FadeUp>
+          <FadeUp delay={0.2}>
+            <p className="pt-4 text-xl">
+              Amoureux de la tech, je conçois des applications mobiles et web
+              ainsi que des back-ends solides.
+            </p>
+          </FadeUp>
+          <FadeUp delay={0.3}>
+            <p className="pt-4 text-xl">
+              J’utilise des technologies modernes tel que Flutter, Laravel,
+              Vue/Nuxt, Next/React, Tailwind CSS et des pratiques DevOps pour
+              livrer des solutions logicielles complètes, orientées utilisateur
+              et de haute qualité.
+            </p>
+          </FadeUp>
 
-          <NavigationInPage right={rightLink} />
+          <FadeUp delay={0.4}>
+            <NavigationInPage right={rightLink} />
+          </FadeUp>
         </main>
       </DefaultLayout>
     </>
