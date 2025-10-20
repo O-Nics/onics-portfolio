d00549ba0028bc4e5cc1c199d863a4af5aaf9670
O-Nicks <np.planche@gmail.com>
2025-10-20 21:27:24 +0200

    trst

--- components/launchMusic.tsx
+++ components/launchMusic.tsx
@@ -1,28 +1,30 @@
 import { MusicIcon } from "@/components/icons";
-import { useMusic } from "@/contexts/MusicContext";
+// import { useMusic } from "@/contexts/MusicContext";
 type LaunchMusicProps = {
   size?: number;
 };
 export default function LaunchMusic({size = 42} : LaunchMusicProps) {
-  const { isPlaying, toggleMusic, audioLevel } = useMusic();
+  // const { isPlaying, toggleMusic, audioLevel } = useMusic();
 
   // Calculer la taille dynamique basée sur l'audio (kicks)
   // Scale entre 1.0 et 1.8 pour un effet visible
-  const dynamicScale = isPlaying ? audioLevel * 1.2 < 1 ? 1 :  audioLevel * 1.2 : 1;
+  // const dynamicScale = isPlaying ? audioLevel * 1.2 < 1 ? 1 :  audioLevel * 1.2 : 1;
 
   return (
-    <button
-      className={`icon-nav cursor-pointer items-center flex ${isPlaying ? "dark:bg-gray-100/5 bg-gray-100 rounded-full" : ""}`}
-      onClick={toggleMusic}
-    >
-      <MusicIcon
-        className={`icon hover:text-red-500 transition-all ${isPlaying ? "!text-red-500" : "text-default-500"}`}
-        size={size}
-        style={{
-          transform: `scale(${dynamicScale})  `,
-          transition: "transform 0.1s ease-out",
-        }}
-      />
-    </button>
+    <>
+      {/*<button*/}
+      {/*  className={`icon-nav cursor-pointer items-center flex ${isPlaying ? "dark:bg-gray-100/5 bg-gray-100 rounded-full" : ""}`}*/}
+      {/*  onClick={toggleMusic}*/}
+      {/*>*/}
+      {/*  <MusicIcon*/}
+      {/*    className={`icon hover:text-red-500 transition-all ${isPlaying ? "!text-red-500" : "text-default-500"}`}*/}
+      {/*    size={size}*/}
+      {/*    style={{*/}
+      {/*      transform: `scale(${dynamicScale})  `,*/}
+      {/*      transition: "transform 0.1s ease-out",*/}
+      {/*    }}*/}
+      {/*  />*/}
+      {/*</button>*/}
+    </>
   );
 }
