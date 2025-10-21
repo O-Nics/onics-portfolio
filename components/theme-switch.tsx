e334f0a3c0c255769fee571003360cf3bf55d30b
O-Nicks <np.planche@gmail.com>
2025-10-21 15:06:51 +0200

    Update theme-switch.tsx

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -40,7 +40,7 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
   }, [isMounted]);
 
   // Prevent Hydration Mismatch
-  if (!isMounted) return <div className="md:w-[40px] md:h-6 w-[18px] h-5" />;
+  if (!isMounted) return <div className="md:w-[41px] md:h-6 w-[19px] h-5" />;
 
   return (
     <Component
@@ -78,18 +78,18 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
         {isSelected ? (
           <div className="icon-nav">
             {/* Mobile (moins que lg) */}
-            <MoonIcon className="block mb-[1px] md:hidden" size={18} />
+            <MoonIcon className="block mb-[1px] md:hidden" size={19} />
 
             {/* Desktop (lg et plus) */}
-            <MoonIcon className="hidden mb-[1px] md:block icon " size={40} />
+            <MoonIcon className="hidden mb-[1px] md:block icon " size={41} />
           </div>
         ) : (
           <div className="icon-nav">
             {/* Mobile (moins que lg) */}
-            <SunIcon className="block mb-[1px] md:hidden  " size={21} />
+            <SunIcon className="block mb-[1px] md:hidden  " size={19} />
 
             {/* Desktop (lg et plus) */}
-            <SunIcon className="hidden mb-[1px] md:block icon" size={42} />
+            <SunIcon className="hidden mb-[1px] md:block icon" size={41} />
           </div>
         )}
       </div>
